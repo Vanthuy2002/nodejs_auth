86a866239469ac78e7dcd7de00094bdaf742a0a7
Thuy Nguyen <vanthuy101102@gmail.com>
2024-01-05 00:09:25 +0700

    get jsonwebtoken

--- config/envConfig.js
+++ config/envConfig.js
@@ -5,7 +5,10 @@ config()
 const envListConfig = {
   MONGO_URI: process.env.MONGO_URI,
   MONGO_PASS: process.env.MONGO_PASS,
-  PORT: process.env.PORT
+  PORT: process.env.PORT,
+  TOKEN_SECRET: process.env.TOKEN_SECRET,
+  ACCESS_TOKEN_EXPIRED: process.env.ACCESS_TOKEN_EXPIRED,
+  REFRESH_TOKEN_EXPIRED: process.env.REFRESH_TOKEN_EXPIRED
 }
 
 module.exports = envListConfig
